75f125a72e61bdfa4eca3ed02636960972c2e74d
Joerg Faschingbauer <jf@faschingbauer.co.at>
2022-06-29 07:37:28 +0200

    2022-06-29(1)

--- docs/_downloads/f670281817c6ad1048a6dd96009c54f8/c++03-ostream-shift-operator.cpp
+++ docs/_downloads/f670281817c6ad1048a6dd96009c54f8/c++03-ostream-shift-operator.cpp
@@ -13,7 +13,7 @@ private:
     int _y;
 };
 
-static inline std::ostream& operator<<(std::ostream& s, point p) // <--- "static inline" is another story
+static std::ostream& operator<<(std::ostream& s, point p) // <--- "static inline" is another story
 {
     s << '(' << p.x() << ',' << p.y() << ')';
     return s;
