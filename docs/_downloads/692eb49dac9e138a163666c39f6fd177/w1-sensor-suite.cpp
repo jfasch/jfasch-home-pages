0e203ded70d6c35b908ad812c657b0105fffa2a2
Joerg Faschingbauer <jf@faschingbauer.co.at>
2022-11-21 20:46:38 +0100

    2022-11-21(1)

---
+++ docs/_downloads/692eb49dac9e138a163666c39f6fd177/w1-sensor-suite.cpp
@@ -0,0 +1,65 @@
+#include <gtest/gtest.h>
+
+#include <sensor-w1.h>
+
+#include <cstdlib>
+#include <unistd.h>
+
+#include <stdexcept>
+
+using namespace std;
+
+
+// fixture class: supply a temporary file, plus a method
+// write_temperature() thaht derived classes can call
+struct w1_sensor_suite : public ::testing::Test
+{
+    void SetUp() override
+    {
+        char filename_pattern[] = "w1_sensor_suite-XXXXXX";
+        
+        fd = mkstemp(filename_pattern);
+        if (fd == -1)
+            throw runtime_error("nix mkstemp()");
+
+        filename = filename_pattern;
+    }
+
+    void TearDown() override
+    {
+        if (fd != -1) {
+            close(fd);
+            unlink(filename.c_str());
+        }
+    }
+
+    int fd;
+    string filename;
+
+    void write_temperature(double temperature)
+    {
+        unsigned temp_milli = temperature * 1000;
+
+        string content = to_string(temp_milli);
+
+        off_t pos = ::lseek(fd, 0, SEEK_SET);
+        ASSERT_EQ(pos, 0);
+
+        ssize_t nwritten = ::write(fd, content.c_str(), content.size());
+        ASSERT_EQ(nwritten, content.size());
+    }
+};
+
+TEST_F(w1_sensor_suite, test_read_sensor)
+{
+    W1Sensor sensor(filename);
+    double temperature;
+
+    write_temperature(42.666);                 // <--- change temperature
+    temperature = sensor.get_temperature();    // <--- read temperature
+    ASSERT_FLOAT_EQ(temperature, 42.666);
+
+    write_temperature(36.5);                   // <--- change temperature
+    temperature = sensor.get_temperature();    // <--- read temperature
+    ASSERT_FLOAT_EQ(temperature, 36.5);
+}
