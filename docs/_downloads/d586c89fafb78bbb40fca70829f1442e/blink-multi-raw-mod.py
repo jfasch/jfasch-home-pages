823fb64bb7f0bf0414e402b5dc7c3288b3fb6795
Joerg Faschingbauer <jf@faschingbauer.co.at>
2023-04-15 10:39:15 +0200

    2023-04-15(1)

--- docs/_downloads/d586c89fafb78bbb40fca70829f1442e/blink-multi-raw-mod.py
+++ docs/_downloads/d586c89fafb78bbb40fca70829f1442e/blink-multi-raw-mod.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
-from stuff_raw import *
+from stuff_raw import *          # <-- this is not about "clean code"
 import asyncio
 
 async def main():
-    t1 = asyncio.create_task(blink((11,), 0.5))
-    t2 = asyncio.create_task(blink((10,), 0.8))
-    t3 = asyncio.create_task(blink((27,), 1))
+    tasks = [
+        asyncio.create_task(blink((11,), 0.5)),
+        asyncio.create_task(blink((10,), 0.4)),
+        asyncio.create_task(blink((27,), 0.3)),
+        asyncio.create_task(blink(( 4,), 0.2)),
+        asyncio.create_task(blink(( 2,), 0.1)),
+    ]
 
-    await t1
-    await t2
-    await t3
+    for t in tasks:
+        await t
 
 asyncio.run(main())
