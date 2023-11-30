74249097c27e5fb773fc906b229457cfe352c3c8
Joerg Faschingbauer <jf@faschingbauer.co.at>
2023-11-30 07:20:22 +0100

    2023-11-30(1)

---
+++ docs/_downloads/2c9d2f997eecd8feedf38f3980c9ef94/related-virtual.h
@@ -0,0 +1,42 @@
+#pragma once
+
+namespace related_virtual
+{
+
+class Sensor
+{
+public:
+    virtual ~Sensor() = default;
+    virtual double get_temperature() = 0;
+};
+
+class Sensor1 : public Sensor
+{
+public:
+    Sensor1(double temperature)
+    : _temperature{temperature} {}
+
+    double get_temperature() override { return _temperature; }
+
+private:
+    double _temperature;
+};
+
+class Sensor2 : public Sensor
+{
+public:
+    Sensor2(double temperature)
+    : _temperature{temperature} {}
+
+    double get_temperature() override { return _temperature; }
+
+private:
+    double _temperature;
+};
+
+double poly_get_temperature(Sensor* s)
+{
+    return s->get_temperature();
+}
+
+}
