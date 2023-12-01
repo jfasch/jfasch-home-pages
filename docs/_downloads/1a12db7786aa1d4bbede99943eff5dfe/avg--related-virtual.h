e441d55740d6990ce362aa1cdf843175a8d5483c
Joerg Faschingbauer <jf@faschingbauer.co.at>
2023-12-01 07:26:05 +0100

    2023-12-01(1)

--- docs/_downloads/1a12db7786aa1d4bbede99943eff5dfe/avg--related-virtual.h
+++ docs/_downloads/1a12db7786aa1d4bbede99943eff5dfe/avg--related-virtual.h
@@ -1,17 +1,14 @@
-#include <cstddef>
-
 namespace related_virtual
 {
 
 double average(auto& sensors)
 {
     double sum = 0;
-    std::size_t num = 0;
-    for (auto s: sensors) {
-        sum += s->get_temperature();
+    unsigned long num = 0;
+    for (auto& s: sensors) {
         ++num;
+        sum += get_temperature(s);
     }
-
     return sum/num;
 }
 
