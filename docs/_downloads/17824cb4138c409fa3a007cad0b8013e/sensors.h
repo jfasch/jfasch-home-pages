9b807ac3831f9bfc1408d9cd04f478f9ab962f80
Joerg Faschingbauer <jf@faschingbauer.co.at>
2023-12-04 07:14:11 +0100

    2023-12-04(1)

---
+++ docs/_downloads/17824cb4138c409fa3a007cad0b8013e/sensors.h
@@ -0,0 +1,40 @@
+#pragma once
+
+enum class DerivedType
+{
+    SENSOR1,
+    SENSOR2,
+};
+
+class Sensor
+{
+public:
+    Sensor(DerivedType type) : type{type} {}
+    const DerivedType type;
+};
+
+class Sensor1 : public Sensor
+{
+public:
+    Sensor1(double temperature)
+    : Sensor{DerivedType::SENSOR1},
+      _temperature{temperature} {}
+
+    double get_temperature() { return _temperature; }
+
+private:
+    double _temperature;
+};
+
+class Sensor2 : public Sensor
+{
+public:
+    Sensor2(double temperature)
+    : Sensor{DerivedType::SENSOR2},
+      _temperature{temperature} {}
+
+    double get_temperature() { return _temperature; }
+
+private:
+    double _temperature;
+};
